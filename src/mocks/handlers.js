72cba5a6db09c12a31dfb9cc00ccee114f1ae563
Amro Abdelfatah <amro.utp.pe@gmail.com>
2023-10-16 00:49:55 +0200

    chore(setup): setup the Mock Service Worker server

---
+++ src/mocks/handlers.js
@@ -0,0 +1,21 @@
+import { rest } from 'msw';
+
+export const handlers = [
+  rest.get("http//localhost:3031/scoops", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: "Mint Chip", imagePath: "/images/mint-chip.png" },
+        { name: "Salted Caramel", imagePath: "/images/salted-caramel.png" }
+      ])
+    );
+  }),
+
+  rest.get("http//localhost:3031/toppings", (req, res, ctx) => {
+  return res(
+    ctx.json([
+      { name: "M&Ms", imagePath: "/images/m-and-ms.png" },
+      { name: "Hot fudge", imagePath: "/images/hot-fudge.png" }
+    ])
+  )
+  })
+]
